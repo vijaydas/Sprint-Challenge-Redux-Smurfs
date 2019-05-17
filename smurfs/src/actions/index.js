4df17fbc84a28793c60f0bdf992d5f78df8cd96e
Vijay Das <vijaytdas@gmail.com>
2019-05-17 10:54:41 -0500

    add axios get in actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,16 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+import axios from 'axios';
+
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
+
+// export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+// export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+// export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +23,19 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_SMURFS_START });
+  axios
+  .get('http://localhost:3333/smurfs')
+  .then( res => {
+    dispatch({
+      type: FETCH_SMURFS_SUCCESS,
+      payload: res.data
+    })
+  })
+  .catch( err => dispatch({
+    type: FETCH_SMURFS_FAILURE,
+    payload: err
+  }))
+}
